dae22a5fa238307df2301bcfb792e02c0f821ba6
mgladkov <mgladkov@abz-1.ru>
2019-05-20 23:34:30 +0300

    Задача 6 + маленький рефактор

--- HW2/index.js
+++ HW2/index.js
@@ -1,34 +1,41 @@
 'use strict';
 
+function userInput(message, min, max) {
+    let result = 0;
+
+    while(true) {
+        result = parseInt(prompt(message, 0));
+        if(!isNaN(result) && result >= min && result <= max)
+            break
+    }
+
+    return result;
+}
+
 function task1() {
     let choice = 0
       , again  = true;
 
-    while(again) {
-        choice = parseInt(prompt(
-            'Задача №1\n\nВведите номер месяца 1 - 12', 1
-        ));
-
-        switch(true) {
-            case choice === 12 || choice == 1 || choice === 2: 
-                alert('Зима'); 
-                again = false; 
-                break;
-            case 3 >= choice || choice <= 5: 
-                alert('Весна'); 
-                again = false; 
-                break;
-            case 6 >= choice || choice <= 8: 
-                alert('Лето'); 
-                again = false; 
-                break;
-            case 9 >= choice || choice <= 11: 
-                alert('Осень'); 
-                again = false; 
-                break;
-            default: 
-                alert('Коллеги, просил же - число от 1 до 12');
-        }
+    choice = userInput('Задача №1\n\nВведите номер месяца 1 - 12', 1, 12);
+    switch(true) {
+        case choice === 12 || choice == 1 || choice === 2: 
+            alert('Зима'); 
+            again = false; 
+            break;
+        case 3 >= choice || choice <= 5: 
+            alert('Весна'); 
+            again = false; 
+            break;
+        case 6 >= choice || choice <= 8: 
+            alert('Лето'); 
+            again = false; 
+            break;
+        case 9 >= choice || choice <= 11: 
+            alert('Осень'); 
+            again = false; 
+            break;
+        default: 
+            alert('Коллеги, просил же - число от 1 до 12');
     }
 }
 
@@ -38,32 +45,20 @@ function task2() {
       , m       = ['дециметрах', 'километрах', 'метрах', 'миллиметрах', 'сантиметрах']
       , message = 'Длина отрезка: ';
 
-    while(true) {
-        if(0 == unit) {
-            unit = parseInt(prompt(
-                'Задача №2\n \
-                Выберите единицу измерения:\n \
-                1 — дециметр\n \
-                2 — километр\n \
-                3 — метр\n \
-                4 — миллиметр\n \
-                5 — сантиметр', 1
-            ));
+    unit = userInput(
+        'Задача №2\n \
+        Выберите единицу измерения:\n \
+        1 — дециметр\n \
+        2 — километр\n \
+        3 — метр\n \
+        4 — миллиметр\n \
+        5 — сантиметр', 1, 5
+    );
 
-            if(isNaN(unit) || 1 > unit || unit > 5) {
-                unit = 0;
-                continue;
-            }
-        }
-
-        length = parseInt(prompt(
-            'Задача №2\n \
-            Введите длину отрезка в ' + m[unit - 1], 1
-        ));
-
-        if(isNaN(length) || length < 0) continue;
-        else break;
-    }
+    length = userInput(
+        'Задача №2\n \
+        Введите длину отрезка в ' + m[unit - 1], 1, 1000000000
+    );
 
     switch(unit) {
         case 1:
@@ -88,28 +83,12 @@ function task2() {
 
 function task3() {
     let input     = 0
-      , canceled  = false
       , message   = ''
-      , represent = ['однозначное', 'двузначное', 'трехзначное']
-      , again     = true;
+      , represent = ['однозначное', 'двузначное', 'трехзначное'];
 
-    while(again) {
-        input = parseInt(prompt(
-            'Домашняя работа №2\n\nВведите число от -999 до 999', 0
-        ));
-
-        if(isNaN(input)) {
-            alert('Не хотите - не надо, пока!');
-            canceled = true;
-            break;
-        }
-
-        if(input >= -999 || input <= 999) {
-            again = false;
-        }
-    }
-
-    if(canceled) return;
+    input = userInput(
+        'Домашняя работа №2\n\nВведите число от -999 до 999', -999, 999
+    );
 
     if(0 === input) {
         message = 'Нулевое число';
@@ -154,16 +133,11 @@ function task5() {
     let input   = 0
       , message = '';
 
-    input = parseInt(prompt(
+    input = userInput(
         'Домашняя работа №2\n\n\
         Попробую оппределить - является год високосным или нет\n\
-        Введите год', 0
-    ));
-
-    if(isNaN(input)) {
-        alert('Не хотите - не надо, пока!');
-        return;
-    }
+        Введите год', 0, 3000
+    );
 
     input = Math.abs(input);
     message = input + ' - этот год не високосный.';
@@ -175,7 +149,38 @@ function task5() {
 }
 
 function task6() {
+    let tarelki  = 10
+      , sredstvo = 3;
 
+    const plural = (n, titles) => {
+        /*
+        http://docs.translatehouse.org/projects/localization-guide/en/latest/l10n/pluralforms.html?id=l10n/pluralforms
+        */
+        return titles[(n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n %10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2)];
+    }
+    
+
+    console.log(
+        'Начали: %s %s и %s средства', 
+        tarelki, 
+        plural(tarelki, ['тарелка', 'тарелки', 'тарелок']), 
+        sredstvo
+    );
+
+    while(tarelki > 0 && sredstvo > 0) {
+        tarelki--;
+        sredstvo -=0.5;
+        console.log('Остаток средства: %s', sredstvo);
+    }
+
+    console.log(
+        'Осталось %s %s, осталось %s средства',
+        tarelki, 
+        plural(tarelki, ['тарелка', 'тарелки', 'тарелок']), 
+        sredstvo
+    );
+
+    alert('Результат в консоли');
 }
 
 let task = 0;
@@ -183,8 +188,8 @@ let task = 0;
 while(true) {
     task = parseInt(prompt(
         'Домашняя работа №2\n\n\
-        Выберите номер задачи (1-6) или нажмите кнопку "отмена"', 1
-    ));
+        Выберите номер задачи (1-6) или нажмите кнопку "отмена"', 1)
+    );
 
     if(isNaN(task)) break;
 
