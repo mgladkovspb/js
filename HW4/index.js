788c699c15380328873335205064b9ac6680b97c
mgladkov <mgladkov@abz-1.ru>
2019-05-26 00:04:57 +0300

    Исправил задачу №4, ДЗ №4

--- HW4/index.js
+++ HW4/index.js
@@ -40,52 +40,122 @@ function task3() {
     return (test1 && test2 && test3.length > 2)? 'Пароль прошел проверку' : 'Пароль не прошел проверку';
 }
 
+/*
+это вариант автокомплита. не совсем то, что нужно
+*/
+
+// function task4() {
+//     let input = document.getElementById('myInput')
+
+//     input.addEventListener('keyup', (e) => {
+//         let userInput   = e.target.value
+//           , inputLength = userInput.length
+//           , typo        = false
+//           , dropdown    = document.getElementById('myDropdown');
+
+//         if(e.target.value.length < 2) {
+//             dropdown.classList.remove('show');
+//             return;
+//         }
+
+//         let match = document.dictionary.filter(
+//             value => RegExp('^' + userInput, 'i').test(value)
+//         ) || [];
+
+//         if(match.length === 0) {
+//             match = document.dictionary.filter(
+//                 value => RegExp('^' + userInput.substring(0, inputLength - 1) + '\\S', 'i').test(value)
+//             ) || [];
+//             typo  = true;
+//         }
+
+//         dropdown.innerHTML = '';
+//         match.forEach(element => {
+//             let template = '<span class="ok">'; 
+//             if(!typo) {
+//                 template += element.substring(0, inputLength) + '</span>';
+//             } else {
+//                 template += element.substring(0, inputLength - 1) + '</span>';
+//                 template += '<span class="typo">' + element.substring(inputLength - 1, inputLength) + '</span>';
+//             }
+//             template += element.substring(inputLength);
+//             dropdown.innerHTML += `<a href="#">${template}</a>`;
+//         });
+
+//         dropdown.childNodes.forEach(node => {
+//             node.addEventListener('click', (e) => {
+//                 input.value = e.target.innerText;
+//                 dropdown.innerHTML = '';
+//                 dropdown.classList.remove('show');
+//             });
+//         });
+//         dropdown.classList.add('show');
+//     });
+// }
+
+/*
+почти строго по задаче
+*/
+
 function task4() {
-    let input = document.getElementById('myInput')
+    let input = document.getElementById('myInput');
+    input.addEventListener('change', (e) => {
+        let userInput = e.target.value
+          , dropdown  = document.getElementById('myDropdown');
 
-    input.addEventListener('keyup', (e) => {
-        let userInput   = e.target.value
-          , inputLength = userInput.length
-          , typo        = false
-          , dropdown    = document.getElementById('myDropdown');
-
-        if(e.target.value.length < 2) {
-            dropdown.classList.remove('show');
+        if(userInput.length === 0)
             return;
-        }
 
-        let match = document.dictionary.filter(
-            value => RegExp('^' + userInput, 'i').test(value)
-        ) || [];
+        const filterFn = (value) => {
+            if(value.length !== userInput.length)
+                return false;
 
-        if(match.length === 0) {
-            match = document.dictionary.filter(
-                value => RegExp('^' + userInput.substring(0, inputLength - 1) + '\\S', 'i').test(value)
-            ) || [];
-            typo  = true;
-        }
+            let reg = new RegExp('(?=[' + value + '])\\S', 'ig')
+              , res = userInput.match(reg);
 
-        dropdown.innerHTML = '';
-        match.forEach(element => {
-            let template = '<span class="ok">'; 
-            if(!typo) {
-                template += element.substring(0, inputLength) + '</span>';
-            } else {
-                template += element.substring(0, inputLength - 1) + '</span>';
-                template += '<span class="typo">' + element.substring(inputLength - 1, inputLength) + '</span>';
+            if(res === null)
+                return false;
+
+            // проверка по % совпавших букв
+            if(res.length * 100 / value.length < 80)
+                return false;
+
+            // проверка по % совпадения букв на местах
+            let matches = 0;
+            for(let i = 0, len = value.length; i < len; i++) {
+                if(value[i].toLowerCase() === userInput[i].toLowerCase())
+                    matches++;
             }
-            template += element.substring(inputLength);
-            dropdown.innerHTML += `<a href="#">${template}</a>`;
-        });
 
-        dropdown.childNodes.forEach(node => {
-            node.addEventListener('click', (e) => {
-                input.value = e.target.innerText;
-                dropdown.innerHTML = '';
-                dropdown.classList.remove('show');
+            if(matches * 100 / value.length < 65)
+                return false;
+
+            return true;
+        }
+
+        let match = document.dictionary.filter(filterFn) || [];
+        if(match.length > 0) {
+            dropdown.innerHTML = '<p>Возможно вы имели ввиду...</p>';
+            match.forEach((element) => {
+                let template = '';
+                for(let i = 0, len = element.length; i < len; i++) {
+                    template += `<span class="${ (element[i].toLowerCase() === userInput[i].toLowerCase())? 'ok' : 'typo' }">${ element[i] }</span>`;
+                }
+                dropdown.innerHTML += `<a href="#" >${ template }</a>`;
             });
-        });
-        dropdown.classList.add('show');
+
+            dropdown.childNodes.forEach(node => {
+                if(node.tagName.toLowerCase() !== 'a')
+                    return;
+    
+                node.addEventListener('click', (e) => {
+                    input.value = e.target.text;
+                    dropdown.innerHTML = '';
+                    dropdown.classList.remove('show');
+                });
+            });
+            dropdown.classList.add('show');
+        }
     });
 }
 
