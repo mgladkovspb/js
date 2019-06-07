9f40abde61a66f0466f137aa0e94446b7a9e4e03
Гладков Максим <mgladkov@abz-1.ru>
2019-06-07 18:15:39 +0300

    HW10

--- HW10/index.js
+++ HW10/index.js
@@ -1,5 +1,117 @@
 'use strict';
 
+let Course  = function(title = 'Без названия', teacher) {
+    this.title   = title;
+    this.teacher = teacher;
+    this.toString = () => {
+        //return `Курс: ${this.title}, ${(!this.teacher instanceof Teacher)? '' : this.teacher.toString() }`; 
+        return `Курс: ${this.title}`; 
+    }
+};
+
+let Teacher = function(place = 'учебное заведение не указано', name = 'имя не указано', age = 'не указан', sex = 'не указан', interests = []) {
+    Person.call(this, name, age, sex, interests);
+
+    this.group;
+    this.place = place;
+    this.toString = () => {
+        let template = `Преподаватель: ${this.name}. 
+            Возраст: ${this.age} ${this._plural(this.valueOf(), ['лет', 'год', 'лет'])}. 
+            Пол: ${this.sex}. 
+            Интересы: ${this.interests.toString() || '- '}. 
+            Обучает в ${this.place}.`;
+        return template
+            .replace(/[\r\n\t\f\v]/gm, "")
+            .replace(/[\\s ]{2,}/gm, '')
+    }
+}
+
+let Group = function(name = 'Не указана.', students = []) {
+    let _students = [];
+
+    for(let i = 0; i < students.length; i++)
+        this.addStudent(students[i]);
+
+    this.name = name;
+    this.addStudent = (student) => {
+        if(!student instanceof Student) {
+            return console.log('Это не студент!');
+        }
+
+        let index = _students.indexOf(student);
+        if(index !== -1) {
+            return console.log('Этот студент уже в группе!')
+        }
+
+        _students.push(student);
+    }
+
+    this.removeStudent = (student) => {
+        if(!student instanceof Student) {
+            return console.log('Это не студент!');
+        }
+
+        let index = _students.indexOf(student);
+        if(index === -1) {
+            return console.log('Этот студент не входит в группу!')
+        }
+
+        _students.splice(index, 1);
+    }
+
+    this.students = () => [..._students]
+
+    this.toString = () => {
+        return this.name;
+    }
+
+    this.valueOf = () => {
+        return _students.length || 0;
+    }
+};
+
+let Person = function(name = 'Имя не указано', age = 'не указан', sex = 'не указан', interests = []) {
+    this.name      = name;
+    this.age       = age;
+    this.sex       = sex;
+    this.interests = [...interests];
+    
+    this._plural = (n, titles) => {
+        return titles[(n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n %10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2)];
+    }
+
+    this.toString = () => {
+        let template = `Человек: ${this.name}. 
+            Возраст: ${this.age} ${this._plural(this.valueOf(), ['лет', 'год', 'лет'])}. 
+            Пол: ${this.sex}. 
+            Интересы: ${this.interests.toString() || '- '}`;
+        return template
+            .replace(/[\r\n\t\f\v]/gm, "")
+            .replace(/[\\s ]{2,}/gm, '')
+    }
+
+    this.valueOf = () => {
+        return parseInt(this.age) || 0;
+    }
+}
+
+let Student = function(place = 'учебное заведение не указано', name = 'имя не указано', age = 'не указан', sex = 'не указан', interests = []) {
+    Person.call(this, name, age, sex, interests);
+
+    this.place = place;
+
+    this.toString = () => {
+        let template = `Студент: ${this.name}. 
+            Возраст: ${this.age} ${this._plural(this.valueOf(), ['лет', 'год', 'лет'])}. 
+            Пол: ${this.sex}. 
+            Интересы: ${this.interests.toString() || '- '}. 
+            Обучается в ${this.place}.`
+        return template
+            .replace(/[\r\n\t\f\v]/gm, "")
+            .replace(/[\\s ]{2,}/gm, '')
+    }
+}
+
 function task1() {
     let Product = function(name = '', price = 0) {
         this.name = name;
@@ -59,19 +171,152 @@ function task1() {
 }
 
 function task2() {
-    
+    let p1 = new Person('Иван', 25, 'м', ['Музыка', 'Программирование']);
+    console.log(p1.toString());
+    console.log((p1 > 25)? 'Больше 25 лет.' : 'Меньше или равно 25 лет.');
+
+    let s1 = new Student('ITMO', 'Андрей', 19, 'м', ['Музыка', 'Программирование']);
+    console.log(s1.toString());
+    console.log((s1 > 25)? 'Больше 25 лет.' : 'Меньше или равно 25 лет.');
 }
 
 function task3() {
-    
+    let User = function(name = 'Имя не указано') {
+        this.name = name;
+        this._plural = (n, titles) => {
+            return titles[(n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n %10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2)];
+        }
+        this.toString = () => {
+            return `Пользователь: ${this.name}`;
+        }
+    }
+
+    User.anonymous = function() {
+        return new User('anonymous');
+    }
+
+    User.withAge = function(name, age) {
+        let user = new User(name);
+        user.age = age;
+        user.toString = function() {
+            return `Пользователь: ${this.name}. Возраст: ${this.age} ${this._plural(this.age || 0, ['лет', 'год', 'лет'])}.`;
+        }
+        return user;
+    }
+
+    let u1 = new User();
+    console.log('Обычный пользователь с не заданным именем: %o', u1.toString());
+    let u2 = User.anonymous();
+    console.log('Анонимный пользователь: %o', u2.toString());
+    let u3 = User.withAge('Максим', 40);
+    console.log('Пользователь с добавленным свойством age: %o', u3.toString());
 }
 
 function task4() {
-    
+    /*
+    1.
+    Объект Number является объектом-обёрткой, позволяющей вам работать с числовыми значениями. Объект Number создаётся через конструктор Number().
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number
+    */
+
+    /*
+    2.
+    Объект ArrayBuffer используется для работы с бинарными данными. Он представляет собой ссылку на поток "сырых" двоичных данных, 
+    однако работать с ними напрямую возможности не дает. Вместо этого, вы можете создать типизированный массив или объект DataView, 
+    который можно использовать для чтения и записи данных в ArrayBuffer. 
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
+    */
+
+    /*
+    3.
+    Объект JSON содержит методы для разбора объектной нотации JavaScript (JavaScript Object Notation — сокращённо JSON) и преобразования значений в JSON.
+    Его нельзя вызвать как функцию или сконструировать как объект, и кроме своих двух методов он не содержит никакой интересной функциональности.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON
+    */
+
+    /*
+    4.
+    Объект Promise (обещание) используется для отложенных и асинхронных вычислений. 
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    */
+
+    /*
+    5.
+    Символ (анг. Symbol) — это уникальный и неизменяемый тип данных, который может быть использован как идентификатор для свойств объектов. 
+    Символьный объект (анг. symbol object) — это объект-обёртка (англ. wrapper) для примитивного символьного типа.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+    */
+
+    /*
+    6.
+    Объект WeakMap — коллекция пар ключ-значение. В качестве ключей могут быть использованы только объекты, а значения могут быть произвольных типов.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
+    */
+
+    /*
+    7.
+    Объект WeakSet - коллекция, элементами которой могут быть только объекты. Ссылки на эти объекты в WeakSet являются слабыми. 
+    Каждый объект может быть добавлен в WeakSet только один раз.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
+    */
+
+    /*
+    8.
+    Объект WeakSet - коллекция, элементами которой могут быть только объекты. Ссылки на эти объекты в WeakSet являются слабыми. 
+    Каждый объект может быть добавлен в WeakSet только один раз.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
+    */
+
+    /*
+    9.
+    Объекты Set позволяют вам сохранять уникальные значения любого типа, как примитивы, так и другие типы объектов.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Set
+    */
+
+    /*
+    10.
+    Объект Atomics предоставляет атомарные операции как статические методы. Используется вместе с объектом SharedArrayBuffer.
+    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Atomics
+    */
+    console.log('См. комментарии в функции task4()...');
 }
 
 function task5() {
+    let course = new Course('2019_05_JS');
+    let teacher = new Teacher('ITMO', 'Александр Разыграев');
+
+    course.teacher = teacher;
     
+    let group = new Group('NodeJS апрель - июль');
+    group.addStudent(new Student('ITMO', 'Деонизия Адулайевна'));
+    group.addStudent(new Student('ITMO', 'Егор Николаевич'));
+    group.addStudent(new Student('ITMO', 'Максим Юрьевич'));
+    group.addStudent(new Student('ITMO', 'Алексей Дмитриевич'));
+    group.addStudent(new Student('ITMO', 'Ксения Сергеевна'));
+    group.addStudent(new Student('ITMO', 'Святослав Вячеславович'));
+    group.addStudent(new Student('ITMO', 'Екатерина Александровна'));
+    group.addStudent(new Student('ITMO', 'Дмитрий Владимирович Малегов'));
+    group.addStudent(new Student('ITMO', 'Алексей Валерьевич'));
+    group.addStudent(new Student('ITMO', 'Кирилл Михайлович'));
+    group.addStudent(new Student('ITMO', 'Роман Владимирович'));
+    group.addStudent(new Student('ITMO', 'Ксения Алексеевна'));
+    group.addStudent(new Student('ITMO', 'Евгений Александрович'));
+    group.addStudent(new Student('ITMO', 'Виталий Владимирович'));
+    group.addStudent(new Student('ITMO', 'Кирилл Станиславович'));
+    group.addStudent(new Student('ITMO', 'Никита Владимирович'));
+    group.addStudent(new Student('ITMO', 'Дмитрий Владимирович Юрасов'));
+    teacher.group = group;
+
+    console.log(course.toString());
+
+    console.log('Группа: %s, количество студентов: %d, состав группы:', teacher.group.toString(), +teacher.group);
+    let students = group.students();
+    for(let i = 0; i < students.length; i++) {
+        console.log(students[i].toString());
+    }
+    console.log(teacher.toString());
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
